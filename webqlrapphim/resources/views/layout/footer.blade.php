d4e9c8f7927c054c0c4096e2f0a61da8b3477093
tohuyson <huysonto@gmail.com>
2020-07-30 08:14:19 +0700

    thực hiện chức năng quản lí phim

---
+++ webqlrapphim/resources/views/layout/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    Team 01 - CNPM 2020.
+</footer>
---
+++ webqlrapphim/resources/views/layout/header.blade.php
@@ -0,0 +1,43 @@
+<div class="topbar">
+    <!-- LOGO -->
+    <div class="topbar-left">
+        <div class="text-center">
+            <a href="" class="logo"><img src="/images/avt.png" width="32px"
+                                         height="32px"><span>CINEMA <i
+                        class="fas fa-phoenix-framework    "></i></span></a>
+        </div>
+    </div>
+
+    <!-- Button mobile view to collapse sidebar menu -->
+    <div class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <div class="">
+                <div class="pull-left">
+                    <button class="button-menu-mobile open-left waves-effect waves-light">
+                        <i class="md md-menu">
+
+                        </i>
+                    </button>
+                    <span class="clearfix"></span>
+                </div>
+
+                <ul class="nav navbar-nav navbar-right pull-right">
+                    <li class="dropdown top-menu-item-xs">
+                        <a href="" class="dropdown-toggle profile waves-effect waves-light" data-toggle="dropdown"
+                           aria-expanded="true"><img src="/images/avt.png" alt="user-img"
+                                                     class="img-circle"> </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#"><i class="ti-user m-r-10 text-custom"></i> Hồ sơ </a>
+                            </li>
+                            <li><a href="#"><i class="ti-settings m-r-10 text-custom"></i>
+                                    Đổi mật khẩu </a></li>
+                            <li class="divider"></li>
+                            <li><a href="#"><i class="ti-power-off m-r-10 text-danger"></i>
+                                    Đăng xuất </a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
